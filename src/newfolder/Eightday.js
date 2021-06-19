97c71b5c89593ca0860d707f97d170f59734458c
unknown <mishoxuco2001@gmail.com>
2021-06-19 17:38:57 +0400

    done

---
+++ src/newfolder/Eightday.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import './Eightday.css'
+const Eightday = ({searchValue}) => {
+    let [data, setData] = useState([]);
+
+
+    const key = '53368e0a2dfb4d4493892950211406';
+    
+    useEffect(() => {
+        getData();
+    }, [searchValue])
+
+    const getData = () => {
+        fetch(`http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${searchValue}&days=3&aqi=no&alerts=yes`)
+            .then(response => response.json())
+            .then(dataa => {
+                setData([dataa]);
+                console.log(dataa)
+            })
+    }
+    
+    console.log(searchValue)
+
+
+    return (
+
+        <div className="day8" >
+
+            {data.map(dataa =>
+                <div key={468276}>
+                    <ul >
+                        <p className="sixday">6 day forecast</p>
+                        <li> {dataa.forecast.forecastday[0].date} <img src={`${dataa.current.condition.icon}`}></img> {dataa.current.temp_c}&#8451; {dataa.current.condition.text}</li>
+                        <li> {dataa.forecast.forecastday[1].date} <img src={`${dataa.current.condition.icon}`}></img> {dataa.current.temp_c}&#8451; {dataa.current.condition.text}</li>
+                        <li> {dataa.forecast.forecastday[2].date} <img src={`${dataa.current.condition.icon}`}></img> {dataa.current.temp_c}&#8451; {dataa.current.condition.text}</li>
+                        <li> {dataa.forecast.forecastday[0].date} <img src={`${dataa.current.condition.icon}`}></img> {dataa.current.temp_c}&#8451; {dataa.current.condition.text}</li>
+                        <li> {dataa.forecast.forecastday[1].date} <img src={`${dataa.current.condition.icon}`}></img> {dataa.current.temp_c}&#8451; {dataa.current.condition.text}</li>
+                        <li> {dataa.forecast.forecastday[2].date} <img src={`${dataa.current.condition.icon}`}></img> {dataa.current.temp_c}&#8451; {dataa.current.condition.text}</li>
+                    </ul>
+                </div>)
+            }
+        </div>
+
+    )
+}
+
+
+export default Eightday;
