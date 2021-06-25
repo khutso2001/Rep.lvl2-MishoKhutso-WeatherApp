cc87995f89abe85df83bdb44a759cf3716c68a8e
unknown <mishoxuco2001@gmail.com>
2021-06-25 16:21:15 +0400

    com

--- src/Music-App/Music.js
+++ src/Music-App/Music.js
@@ -1,52 +1,79 @@
 import React, { useState, useEffect } from 'react'
-import "./OneDayWeather.css"
 import Loader from './Components/Loader'
-import Itunes from "./Itunes"
+import './Music.css'
+import ReactAudioPlayer from 'react-audio-player';
+
 const Music = ({ searchValue }) => {
-    const [dataMusic,setDataMusic]=useState([]);
-    const [loading,setLoading]=useState(false);
+    const [dataMusic, setDataMusic] = useState([]);
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
         getMusicData();
     }, [searchValue])
 
     const getMusicData = () => {
-       setLoading(true);
+        setLoading(true);
         fetch(`https://itunes.apple.com/search?term=${searchValue}`)
-    .then(response=>response.json())
-    .then(dataa=>{
-        setDataMusic([dataa]);
-    
+            .then(response => response.json())
+            .then(dataa => {
+                setDataMusic([dataa]);
+                console.log(dataa)
             })
             .catch(err => {
                 setDataMusic(undefined);
             })
-            .finally(()=>{
+            .finally(() => {
                 setLoading(false);
             })
-           
+
     }
     return (
         <div className="Content">
             <div className="weather-oneday">
-                
+
                 {dataMusic && dataMusic.map(dataa =>
-                <Loader isLoading={loading}>
-                    <div key={dataa.id}>
+                    <Loader isLoading={loading}>
+                        <div key={dataa.id}>
 
-                        
-                    <p>{dataa.results[0].artistName}</p>
-                    <Itunes dataa={dataa}/>
-                       
+                            <p>{dataa.results[0].artistName}</p>
+                            <ReactAudioPlayer
+                                src={dataa.results[0].previewUrl}
+                                
+                                controls
+                            />
+                            <p>{dataa.results[1].artistName}</p>
+                            <ReactAudioPlayer
+                                src={dataa.results[1].previewUrl}
+                                
+                                controls
+                            />
+                            <p>{dataa.results[2].artistName}</p>
+                            <ReactAudioPlayer
+                                src={dataa.results[2].previewUrl}
+                                
+                                controls
+                            />
+                             <p>{dataa.results[3].artistName}</p>
+                            <ReactAudioPlayer
+                                src={dataa.results[3].previewUrl}
+                                
+                                controls
+                            />
+                            <p>{dataa.results[4].artistName}</p>
+                            <ReactAudioPlayer
+                                src={dataa.results[4].previewUrl}
+                                
+                                controls
+                            />
+                          
+                        </div></Loader>
+                )
 
-                    </div></Loader>
-                    )
-                    
                 }
-                
+
             </div>
-           
-           
-           
+
+
+
         </div>
     )
 
